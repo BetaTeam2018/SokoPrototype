2a77a715650b961f87c12037a8532de2ea7bc547
kristofgyori <gykristof0206@gmail.com>
2018-04-23 12:47:18 +0200

    branch előtt egy commit

--- SokoPrototype/src/proto/Menu.java
+++ SokoPrototype/src/proto/Menu.java
@@ -3,15 +3,20 @@ package proto;
 import java.util.ArrayList;
 
 public class Menu {
+	
 	private static void printTheSubtitle() {
 		System.out.println(" ========= Prototípus tesztelés by beta =========\n");
 		System.out.println("Adja meg a tesztelni kívánt eset sorszámát!\n");
 	}
 	
 	public static void printMenuList() {
+		
+		printTheSubtitle();
+		
 		ArrayList<String> menuList = new ArrayList<>();
-		menuList.add("A játékos ládákat tol teszt");	// 1.
-		menuList.add("Maradj talpon teszt");			// 2. kapcsolók és csapóajtók tesztjei
+		menuList.add(" *Kilépés*\n");
+		menuList.add("A játékos ládákat tol teszt");	                              // 1.
+		menuList.add("Maradj talpon teszt");										  // 2. kapcsolók és csapóajtók tesztjei
 		menuList.add("A játékos súrlódás miatt meghal");							  // 3.
 		menuList.add("A játékos ládát tol egy célhelyre, majd letolja róla a ládát"); // 4.
 		menuList.add("A játékos ládát tol egy sima mezőre majd egy mézesre");		  // 5.
@@ -20,7 +25,10 @@ public class Menu {
 		
 		int i = 0;
 		for (String str : menuList) {
-			System.out.println(i++ + str);
+			System.out.println(i++ + ". "+ str);
 		}
+		
+		System.out.println(" ================================================\n");
+		
 	}
 }
